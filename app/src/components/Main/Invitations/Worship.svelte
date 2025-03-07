4cd2ab632367b3a5caabc51a869f165cc0dc5861
Hacks-coder <ajheartcry@gmail.com>
2025-03-07 12:18:45 -0800

    testing the layout

--- app/src/components/Main/Invitations/Worship.svelte
+++ app/src/components/Main/Invitations/Worship.svelte
@@ -10,7 +10,7 @@
 </script>
 <section class="@container mt-20 flex flex-col items-center px-5">
     <div class="border-2 border-black w-fit rounded-md px-5 py-3">I want to know Jesus</div>
-    <div class="grid gap-10 place-items-center @3xl:grid-cols-2 @3xl:gap-20">
+    <div class="grid gap-10 @3xl:grid-cols-2 @3xl:gap-20">
         <div class="flex flex-col gap-2 ">
             <Title text = "Meet Our Members" />
             <Small text = 'We are committed to guiding you in deepening your relationship with God and worshiping Him authentically, as outlined in His Word. In John 4:24, Jesus teaches, "God is spirit, and those who worship him must worship in spirit and truth."' />
--- app/src/components/Main/StaticDirs/Hero.svelte
+++ app/src/components/Main/StaticDirs/Hero.svelte
@@ -13,8 +13,8 @@
 
 <section class="@container mt-20 flex justify-center">
     <div class="grid gap-10 @3xl:grid-cols-2">
-        <div class="px-5 flex flex-col gap-3">
-            <p class="text-3xl font-title bg-gradient-to-r from-[#FD7747] to-[#0C2BD7] bg-clip-text text-transparent @md:text-4xl @3xl:text-5xl">WELCOME TO THE CHURCH OF CHRIST EKPAN</p>
+        <div class="px-5 flex flex-col gap-3 @3xl:mt-20">
+            <p class="text-3xl font-title bg-gradient-to-r from-[#FD7747] to-[#0C2BD7] bg-clip-text text-transparent @md:text-4xl @3xl:text-5xl @5xl:text-7xl">WELCOME TO THE CHURCH OF CHRIST EKPAN</p>
             <Small text = "Let us help you know God better and how to worship effectively in spirit and in truth as written in HIs world" />
             <div class="flex gap-2">
                 <Fill text = "Join" />
