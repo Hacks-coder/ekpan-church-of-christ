587a3f630494bb2e1ea5d47d0beda49a32381285
Hacks-coder <ajheartcry@gmail.com>
2025-03-12 00:00:41 -0700

    testing

--- app/src/components/Main/Invitations/Worship.svelte
+++ app/src/components/Main/Invitations/Worship.svelte
@@ -11,7 +11,7 @@
 <section class="@container mt-20  px-5">
     <div class="border-2 border-black w-fit rounded-md px-5 py-3">I want to know Jesus</div>
     <div class="grid place-items-center @3xl:grid-cols-2 @3xl:gap-10">
-        <div class="flex flex-col gap-2 border-4">
+        <div class="flex flex-col gap-2">
             <Title text = "Meet Our Members" />
             <Small text = 'We are committed to guiding you in deepening your relationship with God and worshiping Him authentically, as outlined in His Word. In John 4:24, Jesus teaches, "God is spirit, and those who worship him must worship in spirit and truth."' />
             <Small text = ' This means that true worship goes beyond rituals; it involves engaging sincerely with God through heartfelt devotion and aligning our lives with His truth.' />
--- app/src/components/Main/Texts/Title.svelte
+++ app/src/components/Main/Texts/Title.svelte
@@ -1,5 +1,5 @@
 <script>
-    let {text, className = "text-black font-bold text-lg" } = $props();
+    let {text, className = "text-black font-bold text-lg @md:text-xl @2xl:text-2xl @4xl:text-4xl" } = $props();
 </script>
 
 <p class={className}>{text}</p>
