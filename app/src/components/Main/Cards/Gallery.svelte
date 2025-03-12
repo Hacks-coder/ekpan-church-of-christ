6ab83b8eac51b3d4c24a0786a28ef11d2b2b3802
Hacks-coder <ajheartcry@gmail.com>
2025-03-11 23:55:08 -0700

    fixed the members pictures sections

--- app/src/components/Main/Cards/Gallery.svelte
+++ app/src/components/Main/Cards/Gallery.svelte
@@ -1,35 +1,7 @@
-<script>
-  const galleryOne = [
-    { src: "/members/img1.jpg", alt: "Image 1", w: "w-45 @xl:90", h: "h-30" },
-    { src: "/members/img2.jpg", alt: "Image 2",w: "w-45 @xl:90", h: "h-35" }, 
-    { src: "/members/img3.jpg", alt: "Image 3",w: "w-45 @xl:90", h: "h-30" }  
-  ];
-  // const galleryTwo = [
-  //   { src: "/members/img4.jpg", alt: "Image 4", w: "w-40", h: "h-57" },
-  //   { src: "/members/img5.jpg", alt: "Image 5", w: "w-40", h: "h-40" } 
-  // ];
-</script>
 
-<div class="@container">
-  <div class="flex gap-3 px-2">
-    <div class="flex flex-col gap-2 border-4">
-      {#each galleryOne as { src, alt, w, h}}
-        <div class={`${w} ${h} @xl:w-60 bg-amber-900 bg-cover bg-center rounded-md`}
-             style="background-image: url({src})"
-             role="img"
-             aria-label={alt}>
-        </div>
-      {/each}
-    </div>
 
-    <div class="flex flex-col gap-2">
-      <!-- {#each galleryTwo as { src, alt, w, h }}
-        <div class={`${w} ${h} bg-green-500 bg-cover bg-center rounded-md`}
-             style="background-image: url({src})"
-             role="img"
-             aria-label={alt}>
-        </div>
-      {/each} -->
-    </div>
+<div>
+  <div>
+    <img src="/members/img1.png" alt="" class="">
   </div>
 </div>
--- app/src/components/Main/Invitations/Worship.svelte
+++ app/src/components/Main/Invitations/Worship.svelte
@@ -10,7 +10,7 @@
 </script>
 <section class="@container mt-20  px-5">
     <div class="border-2 border-black w-fit rounded-md px-5 py-3">I want to know Jesus</div>
-    <div class="flex flex-col items-center">
+    <div class="grid place-items-center @3xl:grid-cols-2 @3xl:gap-10">
         <div class="flex flex-col gap-2 border-4">
             <Title text = "Meet Our Members" />
             <Small text = 'We are committed to guiding you in deepening your relationship with God and worshiping Him authentically, as outlined in His Word. In John 4:24, Jesus teaches, "God is spirit, and those who worship him must worship in spirit and truth."' />
